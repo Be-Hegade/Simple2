e05bfd9d61d5d821ccafc7f5c82f544a0cd60a3a
Be-Hegade <95620721+Be-Hegade@users.noreply.github.com>
2022-06-28 16:34:13 +0530

    Update nameParam.groovy

--- vars/nameParam.groovy
+++ vars/nameParam.groovy
@@ -1,4 +1,4 @@
 //def call(String name, int age){ this can be done better in another way
 def call(Map config = [:]){
-     echo "Name is $(config.name) and age is $(config.age)"
+     echo "Name is ${config.name} and age is ${config.age}"
 }
