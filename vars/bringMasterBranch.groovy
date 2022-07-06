1f4af133857ac99d20abdcb4595520ae1e9373aa
Be-Hegade <95620721+Be-Hegade@users.noreply.github.com>
2022-07-06 22:08:58 +0530

    Update bringMasterBranch.groovy

--- vars/bringMasterBranch.groovy
+++ vars/bringMasterBranch.groovy
@@ -1,6 +1,6 @@
-def call(string branchName){
+def call(String branchName){
    script {
           currentBuild.description = "neww"
         }
-       git branch: 'master', url: 'https://github.com/Be-Hegade/Simple2.git'//<token>@github.com/username/repoName.git
+       git branch: branchName, url: 'https://github.com/Be-Hegade/Simple2.git'//<token>@github.com/username/repoName.git
 }
